b519410772581e2ad4fd99de072ec1af999cb219
Danny Bentley <danny_bentley@hotmail.com>
2024-06-11 15:20:35 +0700

    Add project files.

---
+++ dwpEvolution/CommandRevit2Rhino.cs
@@ -0,0 +1,23 @@
+﻿#region Namespaces
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+#endregion Namespaces
+
+namespace dwpEvolution
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CommandRevit2Rhino : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp?.ActiveUIDocument;
+            Document doc = uidoc?.Document;
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ dwpEvolution/appRibbon.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System;
+using System.IO;
+
+namespace dwpEvolution
+{
+    internal class appRibbon : IExternalApplication
+    {       
+
+        public Result OnStartup(UIControlledApplication a)
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string assemblyDir = Path.GetDirectoryName(assemblyPath);
+
+            try
+            {
+                // Create a new tab named "dwp Tools"
+                a.CreateRibbonTab("dwp Rhino");
+
+                // Create first panel "dwp | BIM Help"
+                RibbonPanel dwpHelpPanel = a.CreateRibbonPanel("dwp Rhino", "dwp | Rhino Tools");
+
+                // Add buttons to the first panel
+                CreateButton(dwpHelpPanel, "Health", "BIM Health", assemblyPath, "dwpBeyond.CommandHealth", "dwpBIMHealth.png", assemblyDir);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle exceptions
+                Console.WriteLine("An error occurred: " + ex.Message);
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        private void CreateButton(RibbonPanel panel, string name, string text, string assemblyPath, string className, string imageName, string imagePath)
+        {
+            PushButtonData buttonData = new PushButtonData(name, text, assemblyPath, className);
+            PushButton button = panel.AddItem(buttonData) as PushButton;
+            button.ToolTip = text;
+            button.LargeImage = new BitmapImage(new Uri(Path.Combine(imagePath, imageName)));
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
